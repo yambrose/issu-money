67343ee2b7417fc528a1ec1792845dfd7b4a8afc
TheLivingPepsi <55717063+TheLivingPepsi@users.noreply.github.com>
2025-03-26 16:04:30 -0500

    Fix cursor

--- src/App.tsx
+++ src/App.tsx
@@ -13,42 +13,77 @@ import Youtube from "./svgs/YouTube";
 import Browser from "./svgs/Browser";
 
 const App: React.FC = () => {
+    return (
+        <div className="h-screen flex flex-col select-none overflow-x-hidden items-center">
+            <Nav siteName="give.issu.money" />
+            <IconSection image={Icon} />
 
-  return (
-    <div className="h-screen flex flex-col select-none overflow-x-hidden items-center">
-      
-      <Nav siteName="give.issu.money" />
-      <IconSection image={Icon} />
+            {/* Links Section */}
+            <div className="flex flex-col justify-center xs:w-full md:w-3/4 lg:w-1/2">
+                <div className="flex flex-col xl:flex-row justify-center w-full">
+                    <LinkColumn
+                        headerText="have money?"
+                        buttons={[
+                            {
+                                icon: <PayPal />,
+                                text: "PayPal",
+                                url: "https://paypal.me/TheLivingPepsi",
+                            },
+                            {
+                                icon: <CashApp />,
+                                text: "Cash App",
+                                url: "https://cash.app/$issumoney",
+                            },
+                            {
+                                icon: <Venmo />,
+                                text: "Venmo",
+                                url: "https://venmo.com/u/thelivingpepsi",
+                            },
+                        ]}
+                    />
+                    <LinkColumn
+                        headerText="no money?"
+                        buttons={[
+                            {
+                                icon: <Ad />,
+                                text: "Watch an ad :D",
+                                url: "/by-watching-an-ad",
+                                subtext: "If you have no ad-blockers",
+                                openInNewTab: false,
+                            },
+                            {
+                                icon: <Roblox />,
+                                text: "Play my Roblox Game",
+                                url: "https://www.roblox.com/games/17309179199",
+                                subtext: "If you have Roblox Premium",
+                            },
+                            {
+                                icon: <Youtube />,
+                                text: "Watch my YouTube videos",
+                                url: "https://www.youtube.com/@TheLivingPepsi",
+                                subtext: "If you have YouTube Premium",
+                            },
+                        ]}
+                    />
+                </div>
+                <LinkColumn
+                    headerText="In the EU or truly broke?"
+                    buttons={[
+                        {
+                            icon: <Browser />,
+                            text: "Check out my site",
+                            url: "https://issu.cloud",
+                            subtext: "(I don't beg as desperately here)",
+                        },
+                    ]}
+                />
+            </div>
 
-      {/* Links Section */}
-      <div className="flex flex-col justify-center xs:w-full md:w-3/4 lg:w-1/2">
-        <div className="flex flex-col xl:flex-row justify-center w-full">
-          <LinkColumn headerText="have money?" buttons={
-            [
-              { icon: <PayPal />, text: "PayPal", url: "https://paypal.me/TheLivingPepsi" },
-              { icon: <CashApp />, text: "Cash App", url: "https://cash.app/$issumoney" },
-              { icon: <Venmo />, text: "Venmo", url: "https://venmo.com/u/thelivingpepsi" }
-            ]
-          } />
-          <LinkColumn headerText="no money?" buttons={
-            [
-              { icon: <Ad />, text: "Watch an ad :D", url: "/by-watching-an-ad", subtext: "If you have no ad-blockers", openInNewTab: false },
-              { icon: <Roblox />, text: "Play my Roblox Game", url: "https://www.roblox.com/games/17309179199", subtext: "If you have Roblox Premium" },
-              { icon: <Youtube />, text: "Watch my YouTube videos", url: "https://www.youtube.com/@TheLivingPepsi", subtext: "If you have YouTube Premium" }
-            ]
-          } />
+            <Footer />
+            <MovingBackground isFlipped={true} />
+            <MovingBackground isFlipped={false} />
         </div>
-        <LinkColumn
-          headerText="In the EU or truly broke?"
-          buttons={[{ icon: <Browser />, text: "Check out my site", url: "https://issu.cloud", subtext: "(I don't beg as desperately here)" }]}
-        />
-      </div>
-
-      <Footer />
-      <MovingBackground isFlipped={true}/>
-      <MovingBackground isFlipped={false}/>
-    </div>
-  )
-}
+    );
+};
 
 export default App;
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, useCallback, useContext, useEffect, useState } from 'react';
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
 
 /*
     This is the main context file for the app. It contains several states and functions that are used throughout the app.
@@ -9,51 +15,66 @@ import { createContext, useCallback, useContext, useEffect, useState } from 'rea
 type ContextType = {
     isDarkMode: boolean;
     handleThemeChange: (value: boolean) => void;
-}
+};
 
 const AppContext = createContext<ContextType | undefined>(undefined);
 
-export const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
+export const AppContextProvider = ({
+    children,
+}: {
+    children: React.ReactNode;
+}) => {
     const [isDarkMode, setIsDarkMode] = useState(true);
     const [isMouseVisible, setIsMouseVisible] = useState(false);
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
     const [isHovering, setIsHovering] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
+    const [wipCurrentPage, setWipCurrentPage] = useState("");
 
     /* -- Check if Mobile -- */
-    /* -- Check if Mobile -- */
-useEffect(() => {
-    const mobileCheck = function () {
-        let check = false;
-        (function (a) {
-            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
-                check = true;
-        })(navigator.userAgent || navigator.vendor || (window as any).opera);
-        return check;
-    };
+    useEffect(() => {
+        const mobileCheck = function () {
+            let check = false;
+            (function (a) {
+                if (
+                    /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+                        a
+                    ) ||
+                    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+                        a.substr(0, 4)
+                    )
+                )
+                    check = true;
+            })(
+                navigator.userAgent || navigator.vendor || (window as any).opera
+            );
+            return check;
+        };
 
-    // Set mobile state based purely on user agent detection
-    setIsMobile(mobileCheck());
+        // Set mobile state based purely on user agent detection
+        setIsMobile(mobileCheck());
 
-    return () => {
-        // No cleanup needed since we're not adding event listeners
-    };
-}, []);
+        return () => {};
+    }, []);
 
     /* -- Theme Change -- */
     const handleThemeChange = () => {
-        setIsDarkMode(prevTheme => !prevTheme);
+        setIsDarkMode((prevTheme) => !prevTheme);
     };
 
     /* -- Custom Cursor -- */
-    const handleMouseMove = useCallback((e: MouseEvent) => {
-        if (isMobile) {
-            setIsMouseVisible(false);
-            return;
-        };
-        setMousePosition({ x: e.clientX, y: e.clientY });
-        setIsMouseVisible(true);
-    }, [isMobile]);
+    const handleMouseMove = useCallback(
+        (e: MouseEvent) => {
+            if (isMobile) {
+                setIsMouseVisible(false);
+                return;
+            }
+
+            setMousePosition({ x: e.clientX, y: e.clientY });
+            setIsMouseVisible(true);
+        },
+        [isMobile]
+    );
 
     const handleMouseLeave = useCallback((e: MouseEvent) => {
         if (
@@ -70,14 +91,14 @@ useEffect(() => {
         if (isMobile) {
             setIsMouseVisible(false);
             return;
-        };
+        }
 
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseout', handleMouseLeave);
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseout", handleMouseLeave);
 
         return () => {
-            document.removeEventListener('mousemove', handleMouseMove);
-            document.removeEventListener('mouseout', handleMouseLeave);
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseout", handleMouseLeave);
         };
     }, [handleMouseMove]);
 
@@ -85,42 +106,81 @@ useEffect(() => {
         if (isMobile) {
             setIsMouseVisible(false);
             return;
-        };
+        }
 
-        const hoverableElements = document.querySelectorAll('button, .hover-button');
+        const hoverableElements = document.querySelectorAll(
+            "button, .hover-button"
+        );
 
         const mouseEnter = () => setIsHovering(true);
         const mouseLeave = () => setIsHovering(false);
 
         hoverableElements.forEach((element) => {
-            element.addEventListener('mouseenter', mouseEnter);
-            element.addEventListener('mouseleave', mouseLeave);
+            element.addEventListener("mouseenter", mouseEnter);
+            element.addEventListener("mouseleave", mouseLeave);
         });
 
         return () => {
             hoverableElements.forEach((element) => {
-                element.removeEventListener('mouseenter', mouseEnter);
-                element.removeEventListener('mouseleave', mouseLeave);
+                element.removeEventListener("mouseenter", mouseEnter);
+                element.removeEventListener("mouseleave", mouseLeave);
             });
         };
     }, []);
 
+    /* -- WIP Alert Notifier -- */
+    useEffect(() => {
+        // Function to handle hash changes
+        const handleHashChange = () => {
+            const newPage = window.location.pathname.slice(1);
 
+            if (wipCurrentPage != newPage) {
+                setWipCurrentPage(newPage);
+
+                if (newPage == "by-watching-an-ad") {
+                    alert(
+                        "too lazy to ad(d) this rn. how about u give me money some other way?"
+                    );
+                } else if (newPage == "contact") {
+                    alert(
+                        "too lazy to make this rn. my twitter is @TheLivingPepsi, and sometimes I check bluesky (@issu.cloud). DM me there"
+                    );
+                } else if (newPage != "") {
+                    window.location.href = window.location.origin;
+                }
+            }
+        };
+
+        window.addEventListener("hashchange", handleHashChange);
+
+        handleHashChange();
+
+        return () => {
+            window.removeEventListener("hashchange", handleHashChange);
+        };
+    }, []);
 
     return (
         <AppContext.Provider value={{ isDarkMode, handleThemeChange }}>
             {children}
             {isMouseVisible && (
-                <div className={`custom-pointer ${isHovering && 'is-hovering'}`}
+                <div
+                    className="custom-pointer-hold"
                     style={{
                         top: `${mousePosition.y}px`,
                         left: `${mousePosition.x}px`,
-                        transform: 'translate(-50%, -50%)'
+                        transform: "translate(-50%, -50%)",
                     }}
-                />
+                >
+                    <div
+                        className={`${
+                            (isHovering && "is-hovering ") || " "
+                        }custom-pointer`}
+                    ></div>
+                </div>
             )}
         </AppContext.Provider>
     );
-}
+};
 
 export const useAppContext = () => useContext(AppContext);
