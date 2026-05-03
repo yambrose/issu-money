627ce8271890714867be3e1eae0727bbaa7b2792
yambrose <randomskitztm@gmail.com>
2026-05-03 19:10:07 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -39,6 +39,8 @@ export default defineConfig(
 	{
 		// Override or add rule settings here, such as:
 		// 'svelte/button-has-type': 'error'
-		rules: {}
+		rules: {
+			'svelte/no-navigation-without-resolve': 'off'
+		}
 	}
 );
